043b10bdacb8ce806ec1d39084259c3edca11ce6
Arupa Nanda Swain <140882921+arupa444@users.noreply.github.com>
2025-01-30 20:40:07 +0530

    Delete traveling-site-main directory

--- traveling-site-main/project.js
+++
@@ -1,9 +0,0 @@
-
-// <script type="text/javascript">
-// window.addEventListener("scroll", function(){
-//     var header = document. querySelector ("header");
-
-//     header.classList.toggle("fixedHeader", window.scrollY > 0);
-
-//     })
-// </script>
